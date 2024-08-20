120465b374451e8da610a8480cc4903aabbd94dd
dizizmi <65617120+dizizmi@users.noreply.github.com>
2024-08-20 11:30:29 +0800

    detailed page (no API)

---
+++ components/DetailedItem.tsx
@@ -0,0 +1,225 @@
+import { ThemedText } from '@/components/ThemedText';
+import React, { useState } from 'react';
+import { StyleSheet, View, Image, Text, TouchableOpacity, ScrollView } from 'react-native';
+
+
+type SizeOption = {
+  value: string;
+  label: string;
+  available: boolean;
+};
+const DetailedItem = ({ route }: any) => {
+  const { id } = route.params;
+  const [selectedSize, setSelectedSize] = useState<string | null>(null);
+
+  const sizes: SizeOption[] = [
+      { value: 'UK 4', label: 'UK 4', available: true },
+      { value: 'UK 4.5', label: 'UK 4.5', available: false },
+      { value: 'UK 5', label: 'UK 5', available: false },
+      { value: 'UK 5.5', label: 'UK 5.5', available: false },
+      { value: 'UK 6', label: 'UK 6', available: false },
+      { value: 'UK 6.5', label: 'UK 6.5', available: false },
+      { value: 'UK 7', label: 'UK 7', available: false },
+      { value: 'UK 7.5', label: 'UK 7.5', available: false },
+      { value: 'UK 8', label: 'UK 8', available: false },
+      { value: 'UK 9', label: 'UK 9', available: false },
+      { value: 'UK 10', label: 'UK 10', available: true },
+      { value: 'UK 11', label: 'UK 11', available: false },
+  ];
+
+  const itemDetails = {
+    title: 'Carhartt x Converse Hi',
+    price: '$195',
+    description:
+      'Redesigned in 1922, Chuck Taylor asked the company to create a better shoe with more support and flexibility...',
+    image: 'https://via.placeholder.com/300',
+  };
+
+
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+        
+        <View style={styles.productPage}>
+        <View style={styles.productHeader}>
+          <Text style={styles.productHeaderTitle}>{itemDetails.title}</Text>
+          <Text style={styles.productPrice}>{itemDetails.price}</Text>
+        </View>
+        </View>
+        
+        <View style={styles.productImageContainer}>
+          <Image
+            source={{ uri: itemDetails.image  }}
+            style={styles.productImage}
+            alt={itemDetails.title}
+            />
+        </View>
+
+        <View style={styles.productSizes}>
+          <Text style={styles.sizeLabel}>SIZE :</Text>
+          <View style={styles.sizeOptions}>
+            {sizes.map((size) => (
+              <TouchableOpacity
+                key={size.value}
+                disabled={!size.available}
+                style={[
+                  styles.sizeOption,
+                  !size.available && styles.sizeOptionUnavailable,
+                  selectedSize === size.value && styles.sizeOptionSelected
+                ]}
+                onPress={() => setSelectedSize(size.value)}
+              >
+                <Text>{size.label}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+          <View style={styles.sizeNotAvailable}>
+            <Text style={styles.sizeNotAvailableLink}>Your size not available?</Text>
+          </View>
+        </View>
+
+        <View style={styles.productDescription}>
+          <ThemedText>
+            {itemDetails.description}
+          </ThemedText>
+        </View>
+
+        <View style={styles.productActions}>
+          <TouchableOpacity style={styles.quickCheckout}>
+            <Text>Quick check out</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.wishlist}>
+            <Text>❤️</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 60,
+  },
+
+  productPage: {
+    padding: 16,
+
+  },
+  
+  productHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+
+  productHeaderTitle: {
+    fontSize: 24,
+    margin: 0,
+  },
+
+  productPrice: {
+    fontSize: 20,
+    color: '#333',
+  },
+
+  productImageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center', // Centering image horizontally
+    marginBottom: 20,
+   
+  },
+
+  productImage: {
+    width: 300,
+    height: 300, // Fixed height to maintain aspect ratio
+    resizeMode: 'contain', // Ensure the image scales properly
+  },
+
+  productSizes: {
+    padding: 20
+  },
+
+  sizeLabel: {
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+
+  sizeOptions: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+
+  sizeOption: {
+    width: 60,
+    height: 40,
+    margin: 4,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 14,
+  },
+
+  sizeOptionSelected: {
+    backgroundColor: '#007bff',
+    color: '#fff',
+  },
+
+  sizeOptionUnavailable: {
+    backgroundColor: '#eee',
+    color: '#999',
+  },
+
+  sizeNotAvailable: {
+    marginTop: 8,
+  },
+
+  sizeNotAvailableLink: {
+    color: '#007bff',
+    textDecorationLine: 'underline',
+  },
+
+  productDescription: {
+    padding: 20,
+    marginBottom: 16,
+  },
+
+  productActions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  quickCheckout: {
+    flexGrow: 1,
+    padding: 10,
+    marginRight: 8,
+    marginLeft: 10,
+    marginBottom: 20,
+    backgroundColor: '#007bff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    fontSize: 16,
+    color: '#fff',
+
+  },
+
+  wishlist: {
+    padding: 10,
+    marginRight: 10,
+    marginBottom: 20,
+    backgroundColor: '#fff',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+});
+
+export default DetailedItem;
