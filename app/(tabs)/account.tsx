6fbf1a231ce4caeabc34a0ddb1247c08d8127bce
dizizmi <65617120+dizizmi@users.noreply.github.com>
2024-08-20 11:35:26 +0800

    default account page

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,6 +1,6 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
+
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
@@ -8,44 +8,42 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+        headerImage={
+            <Image
+            source={require('@/assets/images/partial-react-logo.png')}
+            style={styles.reactLogo}
+            />
+        }
+>
+        <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Account</ThemedText>
+
+        </ThemedView>
+        <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">hello</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
+            <ThemedText type="defaultSemiBold">insert Name</ThemedText> 
         </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+        </ThemedView>
+
+        <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">My orders</ThemedText>
         <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
+            Orders
         </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+        </ThemedView>
+
+        <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Account settings</ThemedText>
         <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+            Details
         </ThemedText>
-      </ThemedView>
+        </ThemedView>
+
+        <ThemedView style={styles.logOutContainer}>
+            <ThemedText type="subtitle">logout</ThemedText>
+        </ThemedView>
     </ParallaxScrollView>
   );
 }
@@ -60,6 +58,12 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
+logOutContainer: {
+    gap: 8,
+    marginTop: 50,
+    marginBottom: 8,
+    alignItems: 'center',
+},
   reactLogo: {
     height: 178,
     width: 290,
